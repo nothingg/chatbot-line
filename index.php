3f4178b9868ebc4b334aa70b5a50062b5a02d212
nothingg <bird_aries@hotmail.com>
2017-07-06 20:35:41 +0700

    new

--- index.php
+++ index.php
@@ -17,15 +17,9 @@ if (!is_null($events['events'])) {
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
-			$messages = [{
-                'type' => 'text',
+			$messages = [
+				'type' => 'text',
 				'text' => $text
-            },{
-                'type' => 'sticker',
-				'packageId' => '1',
-                'stickerId' => '1'
-            }
-				
 			];
 
             $messages2 = [
@@ -38,7 +32,7 @@ if (!is_null($events['events'])) {
 			$url = 'https://api.line.me/v2/bot/message/reply';
 			$data = [
 				'replyToken' => $replyToken,
-				'messages' => [$messages],
+				'messages' => [$messages ,$messages2],
 			];
 			$post = json_encode($data);
 			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
